343fb9013ed454e403a80d4dce9441ec3932ab8f
arzuagamanuel <71405710+ManuelArzuaga@users.noreply.github.com>
2025-07-31 20:03:48 -0300

    modificacion perfil

--- src/Views/Perfil.jsx
+++ src/Views/Perfil.jsx
@@ -1,14 +1,14 @@
 import {useEffect, useState} from "react"
 import Layout from "../Components/Layout/Layout"
-import {auth, db} from "../Config/Firebase"
+import {db} from "../Config/Firebase"
 import { getDoc,doc,updateDoc } from "firebase/firestore"
 import { useNavigate} from "react-router-dom"
 import { useAuth } from "../Context/AuthContext"
+import { updatePassword } from "firebase/auth"
 
 
 function Perfil(){
 
-  const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
   const [name,setName] = useState("")
   const [apellido,setApellido] = useState("")
@@ -29,7 +29,6 @@ function Perfil(){
 
       if (docsnap.exists()){
         const data = docsnap.data()
-        setEmail(data.email)
         setPassword(data.password)
         setName(data.name)
         setApellido(data.apellido)
@@ -44,20 +43,22 @@ function Perfil(){
     HandleProduct(user)
   },[user])
 
-  function handleSubmit(e){
+  async function handleSubmit(e){
     e.preventDefault()
-    if(!name || !apellido)
+    if(!name || !apellido || !password) 
     {
       setError("Completar los campos")
       return
     }
 
     try {
+      await updatePassword(user,password)
       const docref = doc(db,"Usuarios",user.uid)
-      updateDoc(docref,{name,apellido})
+      await updateDoc(docref,{name,apellido,password})
       
     } catch (error) {
       setError(error.message)
+      return
     }
     
     setTimeout(()=>{
@@ -76,8 +77,10 @@ function Perfil(){
         <form onSubmit={handleSubmit}>
           <label htmlFor="name">Nombre:</label>
           <input type="text" name="name" id="name" value={name} onChange={(e)=>{setName(e.target.value)}}></input>
-           <label htmlFor="name">Apellido:</label>
+          <label htmlFor="name">Apellido:</label>
           <input type="text" name="apellido" id="apellido" value={apellido} onChange={(e)=>{setApellido(e.target.value)}}></input>
+          <label htmlFor="name">Contraseña:</label>
+          <input type="password" name="password" id="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
           <button>Editar Perfil</button>
           {error && <p>{error}</p>}
           {message && <p>{message}</p>}
